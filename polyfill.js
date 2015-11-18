2404004808737469257d953307a08d94675ad386
Jordan Harband <ljharb@gmail.com>
2015-11-17 23:50:03 -0800

    Reference implementation.

---
+++ polyfill.js
@@ -0,0 +1,7 @@
+const toStr = Function.bind.call(Function.call, Object.prototype.toString);
+
+if (!Error.isError) {
+	Error.isError = function isError(arg) {
+		return arg && toStr(arg) === '[object Error]';
+	};
+}
