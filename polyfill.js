d3bf8a43d3267ab52b2ff3f594a019fa73907831
Jordan Harband <ljharb@gmail.com>
2015-11-20 21:35:07 -0800

    Make sure to always return a boolean value.
    Per https://github.com/ljharb/proposal-is-error/commit/2404004808737469257d953307a08d94675ad386#commitcomment-14528192

--- polyfill.js
+++ polyfill.js
@@ -2,6 +2,6 @@ const toStr = Function.bind.call(Function.call, Object.prototype.toString);
 
 if (!Error.isError) {
 	Error.isError = function isError(arg) {
-		return arg && toStr(arg) === '[object Error]';
+		return !!arg && toStr(arg) === '[object Error]';
 	};
 }
