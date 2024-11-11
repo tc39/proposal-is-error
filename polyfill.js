28b8601ceeba93225f07cd639549d491da0dae25
Jordan Harband <ljharb@gmail.com>
2024-11-11 12:03:09 -0800

    remove polyfill
    See #16

--- polyfill.js
+++
@@ -1,7 +0,0 @@
-const toStr = Function.bind.call(Function.call, Object.prototype.toString);
-
-if (!Error.isError) {
-	Error.isError = function isError(arg) {
-		return !!arg && toStr(arg) === '[object Error]';
-	};
-}
